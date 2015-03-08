9c71a320fadc05fdd2ea878548f897643b05007b
Garry T. Williams <gtwilliams@gmail.com>
2015-03-08 14:22:10 -0400

    Refactored for other pages and added Newsletter page.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* Copyright (c) 2015 Garry T. Williams */
+
 var express = require('express');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -5,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -16,21 +17,20 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.gif'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -38,24 +38,26 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
 module.exports = app;
+
+// vim: sw=4 sts=4 ts=8 ai et syn=javascript
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+/* Copyright (c) 2015 Garry T. Williams */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Duplicate Bridge Association of Atlanta' });
+    res.render('index', { title: 'Welcome to the DBAA Home Page' });
+});
+
+/* GET newsletters */
+router.get('/newsletters', function(req, res, next) {
+    res.render('newsletters', { title: 'DBAA Newsletters' });
 });
 
 module.exports = router;
+
+// vim sw=4 sts=4 ts=8 ai et syn=javascript
