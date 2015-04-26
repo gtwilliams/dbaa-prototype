a76d1fea0bff63ade96df9b85674d89904198059
Garry T. Williams <gtwilliams@gmail.com>
2015-04-26 12:59:49 -0400

    Incremental prototypes: E-mail and parameters.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Copyright (c) 2015 Garry T. Williams */
 
 var express = require('express');
+var session = require('express-session');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -10,6 +11,8 @@ var routes = require('./routes/index');
 
 var app = express();
 
+var RedisStore = require('connect-redis')(session);
+
 app.locals.pretty = true;
 
 // view engine setup
@@ -18,10 +21,33 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/images/favicon.gif'));
-app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(logger('short'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use(session({
+    resave: true,
+    saveUninitialzed: true,
+    secret: 'Fia2gom2',
+    store: new RedisStore({
+        host: 'localhost',
+        port:6379
+    })
+}));
+
+/*
+app.use(function (req, res, next) {
+    if (req.cookies !== undefined &&
+        req.cookies['connect.sid'] !== undefined)
+    {
+        console.log(req.cookies['connect.sid']);
+    }
+
+    next(); // call the next middleware
+});
+*/
+
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 /* Copyright (c) 2015 Garry T. Williams */
 
 var express = require('express');
-var router = express.Router();
-var pg     = require('pg');
-var conn   = "postgres://garry:Fia2gom2@localhost/dbaa";
-var client = new pg.Client(conn);
+var router  = express.Router();
+var pg      = require('pg');
+var conn    = "postgres://garry:Fia2gom2@localhost/dbaa";
+var client  = new pg.Client(conn);
+var mailer  = require('nodemailer');
+var mail    = mailer.createTransport();
 
 client.connect();
 
@@ -16,6 +18,29 @@ router.get('/', function(req, res, next) {
 /* GET newsletters */
 router.get('/newsletters', function(req, res, next) {
     res.render('newsletters', { title: 'DBAA Newsletters' });
+});
+
+/* For POST, you need body-parser and parameters show up in
+ * req.body.PARAM.  For GET, they automatically show up in
+ * req.query.PARAM. */
+/* Create a new login ID */
+router.post('/create-new-login', function(req, res, next) {
+    if (req.body.login_name === undefined)
+	return res.render('create-login', {title: 'Create User Login'});
+    mail.sendMail({
+	from: "gtwilliams@gmail.com",
+	to: req.body.login_name,
+	subject: "Create Login on DBAA Web Site",
+	text: "Go here: foo.com"
+    }, function (err, info) {
+	if (err) {
+	    console.log("Oops: " + err);
+	}
+    });
+});
+
+router.get('/create-new-login', function(req, res, next) {
+    res.render('create-login', {title: 'Create User Login'});
 });
 
 /* GET test E-mail link */
