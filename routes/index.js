b52c51d97d559dac088c9ce63d3c4e875544c594
Garry T. Williams <gtwilliams@gmail.com>
2015-04-28 14:16:30 -0400

    Added support for E-mailing user for a sign-up link.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router  = express.Router();
 var config  = require('../config.json');
+var crypto  = require('crypto');
 var pg      = require('pg');
 var conn    = "postgres://" + config.dbuser + ":" + config.dbpass +
 	      "@localhost/dbaa";
@@ -32,9 +33,26 @@ router.get('/newsletters', function(req, res, next) {
 });
 
 router.get('/create-new-login/:id', function(req, res, next) {
+    var q = "SELECT e_mail\n" +
+            "  FROM e_mail_campaign\n" +
+            " WHERE id = $1";
+    client.query(q, [ req.params.id ], function (err, result, done) {
+	if (result.rows.length == 0) {
+	    var err = new Error('Not Found');
+	    err.status = 404;
+	    next(err);
+	}
+
+	/* Show sign-up form */
+	res.render('/create-login', {
+	    title  : 'Create My DBAA Login',
+	    id     : req.params.id,
+	    e_mail : result.rows[0].id
+	});
+    });
 });
 
-router.post('/create-new-login/:id', function(req, res, next) {
+router.post('/create-login', function(req, res, next) {
 });
 
 /* Create a new login ID */
@@ -44,32 +62,56 @@ router.post('/create-new-login', function(req, res, next) {
     if (req.body.login_name === undefined || req.body.login_name == "")
 	return res.render('create-login', {title: 'Create User Login'});
 
-    // get an ID recorded to but in the URL
+    /* We record a unique ID associated with this E-mail address and
+     * make that the URL the user will use coming back to us.  We know
+     * it's him this way. */
     var id = crypto.randomBytes(20).toString('hex');
+    var q  = "INSERT INTO e_mail_campaign\n" +
+	     "    (e_mail, id, name, campaign)\n" +
+             "VALUES ($1, $2, 'Create Login', 0)";
 
-    mail.sendMail({
-	from: "gtwilliams@gmail.com",
-	to: req.body.login_name,
-	subject: "Create Login on DBAA Web Site",
-	text: "Go here: foo.com"
-    }, function (err, info) {
-	if (err) {
-	    console.log("Oops: " + err);
-	    return res.render('mail-send-error',
-		{
-		    title: 'Send E-mail Error',
-		    e_mail: req.body.login_name,
-		    error: err
+    /* Record the unique value to associate with this E-mail address. */
+    client.query(q, [ req.body.login_name, id ],
+       	function (err, result, done) {
+	    if (err)
+		return res.render('error', err);
+
+	    /* Send the E-mail with the unique, unguessable value that
+	     * we can look up in the database to get the E-mail
+	     * address later. */
+	    mail.sendMail({
+		from: "Garry Williams <gtwilliams@gmail.com>",
+		to: req.body.login_name,
+		subject: "Create Login on DBAA Web Site",
+		text: "Go here to complete your sign-up: " +
+		    req.protocol + "://" + req.hostname +
+		    req.baseUrl + "/" + id
+	    }, function (err, info) {
+		if (err) {
+		    console.log("send E-mail failed: " + err);
+		    res.render('mail-send-error', {
+			title  : 'Send E-mail Error',
+			e_mail : req.body.login_name,
+			error  : err
+		    });
+
+		    /* If the E-mail message never went out, no need
+		     * to remember the E-mail address. */
+		    var q = "DELETE FROM e_mail_campaign\n" +
+		            " WHERE id = $1";
+		    client.query(q, [ id ],
+			function (err, result, done) {
+			    if (err)
+				return res.render('error', err);
+		    });
 		}
-	    );
+	    });
 	}
+    );
 
-	return res.render('sent-mail',
-	    {
-		e_mail: req.body.login_name,
-		title: 'E-mail Sent'
-	    }
-	);
+    return res.render('sent-mail', {
+	e_mail : req.body.login_name,
+	title  : 'E-mail Sent'
     });
 });
 
