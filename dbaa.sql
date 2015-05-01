f1d663e9fecb5f47ce11051c07d748b5511e9a71
Garry T. Williams <gtwilliams@gmail.com>
2015-05-01 16:42:37 -0400

    Add initial directory and phone number tables.  Still need triggers.

--- dbaa.sql
+++ dbaa.sql
@@ -20,4 +20,29 @@ CREATE TABLE e_mail_campaign (
     created  TIMESTAMP DEFAULT NOW()
 );
 
+CREATE SEQUENCE directory_seq;
+
+CREATE TABLE directory (
+    id       INTEGER NOT NULL PRIMARY KEY DEFAULT NEXTVAL('directory_seq'),
+    name     VARCHAR(100) NOT NULL,
+    acbl     VARCHAR(7), -- player number
+    e_mail   VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE phone_number (
+    directory INTEGER NOT NULL FOREIGN KEY REFERENCES directory(id),
+    type      VARCHAR(30) NOT NULL DEFAULT 'Cell',
+    number    VARCHAR(10)
+);
+
+CREATE INDEX phone_number_directory ON phone_number(directory);
+
+CREATE TABLE phone_number_types (
+    sort INTEGER NOT NULL DEFAULT 0,
+    name VARCHAR(30)
+);
+
+INSERT INTO phone_number_types (sort, name) VALUES (1, 'Home');
+INSERT INTO phone_number_types (sort, name) VALUES (2, 'Cell');
+
 -- vim: sw=4 sts=4 ts=8 et ai syn=sql
