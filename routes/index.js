27e51108d0d8ecd8d51516e27af100c8945ea25c
Garry T. Williams <gtwilliams@gmail.com>
2015-05-02 17:21:07 -0400

    Initial version of forgot password, reset it.

--- routes/index.js
+++ routes/index.js
@@ -258,6 +258,159 @@ router.get('/create-new-login', function(req, res, next) {
     res.render('create-new-login', {title: 'Create User Login'});
 });
 
+/* Someone got here from an E-mail message. */
+router.get('/reset-password/:id', function (req, res, next) {
+    var q = "SELECT id, e_mail\n" +
+            "  FROM e_mail_campaign\n" +
+            " WHERE id = $1";
+    db.query(q, [ req.params.id ], function (err, result, done) {
+        if (result.rows.length == 0) {
+            var e = new Error('Not Found');
+            e.status = 404;
+            return next(e);
+        }
+
+        /* Show reset form */
+        res.render('reset-password', {
+            title  : 'Reset My DBAA Web Site Password',
+            id     : result.rows[0].id,
+            e_mail : result.rows[0].e_mail
+        });
+    });
+});
+
+/* Reset my password! */
+router.get('/reset-password', function(req, res, next) {
+    if (req.body.e_mail === undefined || req.body.e_mail == "") {
+        var err = new Error('Not found');
+        err.status = 404;
+        return next(err);
+    }
+
+    /* Let's look you up first -- you must have an account here to
+     * reset a password. */
+    var q = "SELECT id, name, e_mail\n" +
+            "  FROM users\n" +
+            " WHERE login = $1";
+    db.query(q, [ req.params.e_mail ], function (err, result, done) {
+        if (err) {
+            err.status = 500;
+            return next(err);
+        }
+
+        /* Nice try.  No login here. */
+        if (result.rows.length == 0) {
+            return res.render('forgot-error',
+                { title: 'Unknown E-mail Address'});
+        }
+
+        /* Now that we found you, we need to send you E-mail to reset
+           that password. */
+        var id = crypto.randomBytes(20).toString('hex');
+        q = "INSERT INTO e_mail_campaign\n" +
+            "    (e_mail, id, name, campaign)\n" +
+            "VALUES ($1, $2, 'Reset Password', $3)";
+        db.query(q, [ result.rows[0].e_mail, id, result.rows[0].id ],
+                function (err, result, done)
+        {
+            if (err) {
+                err.status = 500;
+                return next(err);
+            }
+
+            mail.sendMail({
+                from: "Garry Williams <gtwilliams@gmail.com>",
+                to: result.rows[0].e_mail,
+                subject: "Reset Tour DBAA Web Site Password",
+                text: "Dear " + result.rows[0].name +
+                      ",\n Go here to reset your password: " +
+                      req.protocol + "://" + req.get('host') +
+                      "/reset-password/" + id + "\n\n" +
+                      "If you did not request a change of " +
+                      "your password, just ignore this E-mail."
+            }, function (err, info) {
+                if (err) {
+                    console.log("E-mail failed: " + err);
+                    err.status = 500;
+                    return next(err);
+                }
+
+                else {
+                    return res.render('sent-mail',
+                        {
+                            title: "E-mail Sent"
+                        });
+                }
+            });
+        });
+    });
+});
+
+/* Final step: reset the password. */
+router.post('/reset-password', function(req, res, next) {
+    /* Validate input data */
+    var q = "SELECT id, e_mail\n" +
+            "  FROM e_mail_campaign\n" +
+            " WHERE id = $1";
+    db.query(q, [ req.body.id ], function (err, result, done) {
+        if (err) {
+            err.status = 500;
+            return next(err);
+        }
+
+        /* The end-user never sees the unique ID in this form -- it's
+         * supplied in a hidden input element.  So, if it's invalid,
+         * we will give a 404 error since it should never happen
+         * unless something fishy is going on out there. */
+        if (result.rows.length == 0) {
+            console.log("can't find id=" + req.body.id +
+                " in the e_mail_canpaign table");
+            var err = new Error('Not Found');
+            err.status = 404;
+            return next(err);
+        }
+
+        /* Again, the user shouldn't have a way to do this since the
+         * browser checks the data before allowing the post.  We will
+         * just issue a 404 since we don't want to be friendly to the
+         * other end on these sorts of errors. */
+        if (req.body.password != req.body.duppassword) {
+            console.log("user's passwords don't match");
+            var err = new Error('Not Found');
+            err.status = 404;
+            return next(err);
+        }
+
+        /* Create the login ID */
+        q = "UPDATE users\n" +
+            "   SET password = $1\n" +
+            " WHERE login = $2";
+        bcrypt.hash(req.body.password, 8, function (err, hash) {
+            if (err) {
+                err.status = 500;
+                return next(err);
+            }
+
+            db.query(q, [ hash, result.rows[0].login ],
+                    function (err, result, done)
+            {
+                if (err) {
+                    console.log("Error trying to insert new login ID");
+                    err.status = 500;
+                    return next(err);
+                }
+
+                req.session.name   = req.body.name;
+                req.session.e_mail = req.body.e_mail;
+                return res.render('index', {
+                    title: 'Welcome to the DBAA Home Page',
+                    login: req.session.name
+                });
+            });
+        });
+    });
+});
+
 module.exports = router;
 
 // vim: sw=4 sts=4 ts=8 ai et syn=javascript
