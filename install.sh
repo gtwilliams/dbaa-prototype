b04382c600c9edddd0d8ac3040bb5c9bf59b2385
Garry T. Williams <gtwilliams@gmail.com>
2015-05-03 10:55:59 -0400

    Initial customizations of bootstrap using lesscss.

--- install.sh
+++ install.sh
@@ -3,12 +3,19 @@
 SERVER_ROOT=${HOME}/myapp
 FILES="app.js config.json"
 DIRS="bin public routes views"
+LESSCSS=lessc
+STYLESHEETS=public/stylesheets
 
 [ -d $SERVER_ROOT ] || mkdir -p $SERVER_ROOT
 
 for f in $FILES; do
     echo "cp $f $SERVER_ROOT"
     cp $f $SERVER_ROOT
+done
+
+# generate style sheets
+for s in $STYLESHEETS/*.less; do
+    $LESSCSS $s > ${s%%.*}.css
 done
 
 for d in $DIRS; do
